5d86356421dd09e3460d37ab9c9275c475bec6b1
zelon <zelon@DESKTOP-2J2O4UE.mshome.net>
2020-11-28 22:44:27 -0500

    Cleanup of borked emdash character

--- src/main/java/com/streetferret/opus/StringUtil.java
+++ src/main/java/com/streetferret/opus/StringUtil.java
@@ -18,7 +18,7 @@ public class StringUtil {
 	}
 
 	public static String cleanAreaName(String name) {
-		return name.replace("_", " ").replace("\"", "").replace("*", "").replaceAll("([A-Za-z])\\s*,\\s*([A-Za-z])",
-				"$1, $2");
+		return name.replace("_", " ").replace("\"", "").replace("*", "").replace("–", "-")
+				.replaceAll("([A-Za-z])\\s*,\\s*([A-Za-z])", "$1, $2");
 	}
 }
