c36728986bf9ef58478577491aeb78a61304d41b
Brian Sperlongano <zelonewolf@gmail.com>
2020-11-30 23:11:33 -0500

    organize imports

--- src/main/java/com/streetferret/opus/OverpassLookup.java
+++ src/main/java/com/streetferret/opus/OverpassLookup.java
@@ -2,13 +2,11 @@ package com.streetferret.opus;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeSet;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 import com.streetferret.opus.osmdb.OSMProtectedAreaRecord;
 import com.streetferret.opus.osmdb.StateProtectedAreaDatabase;
