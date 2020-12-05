d962ca225cb2ce1f81ae5537b4319ee2069de630
Brian Sperlongano <zelonewolf@gmail.com>
2020-12-04 22:58:39 -0500

    Restoring conflation check

--- src/main/java/com/streetferret/opus/osmdb/OSMProtectedAreaLookupResults.java
+++ src/main/java/com/streetferret/opus/osmdb/OSMProtectedAreaLookupResults.java
@@ -51,4 +51,15 @@ public class OSMProtectedAreaLookupResults {
 		List<LocationMatch> removedRecords = nameIndex.remove(rec);
 		records.removeAll(removedRecords);
 	}
+
+	public void removeAssociatedAreas(LocationDatabase db) {
+		db.getMatchMap()
+			.entries()
+			.stream()
+			.map(e -> e.getValue())
+			.filter(LocationMatch::noAreaConflation)
+			.map(LocationMatch::getId)
+			.forEach(this::removeByID);
+		index();
+	}
 }
